1be3036c501c18e5c60dd8093af6f65306e440a2
Fawwaz Abrial Saffa <fawwazabrial.fas@gmail.com>
2025-09-04 19:28:17 +0700

    change theme to ashen and fix clipboard

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Cool file name on top right of screen
+vim.opt.winbar = "%=%m %f"
+
+-- Yank / register to clipboard
+-- Needs a clipboard manager (used 'wl-clipboard' on KDE Arch)
+vim.opt.clipboard = "unnamedplus"
---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "ficcdaf/ashen.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "ashen",
+    },
+  },
+}
