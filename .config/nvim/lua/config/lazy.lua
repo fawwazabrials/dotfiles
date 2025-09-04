b9254f1dc75edbdf39d5a5af6d577dbe70384f1f
Fawwaz Abrial Saffa <fawwazabrial.fas@gmail.com>
2025-09-04 22:22:47 +0700

    add my commonly used languages and c format support

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -19,11 +19,27 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
+    --  fun extras modules
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+
+    -- extras modules for files
+    { import = "lazyvim.plugins.extras.lang.git" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
+
     -- extras modules for web development
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- { import = "lazyvim.plugins.extras.linting.eslint" },
+
+    -- extras modules for other programming languages
+    { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.go" },
 
     -- import/override with your plugins
     { import = "plugins" },
